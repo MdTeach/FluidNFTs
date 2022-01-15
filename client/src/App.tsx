0dfadb7089bf54a502e76723bcff75922a73febd
Abishek Bashyal <abishek_bashyal@outlook.com>
2022-01-15 15:59:57 +0545

    client added

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import { useEffect } from "react";
+
+function App() {
+  useEffect(() => {
+    (async () => {})();
+  }, []);
+  return (
+    <div className="App">
+      <h1>Abishek</h1>
+    </div>
+  );
+}
+
+export default App;
